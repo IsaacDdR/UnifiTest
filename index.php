2643ee4e87adc7d408a5ab3d4f9ca42a2cb06e71
IsaacDdR <isaacovic00@gmail.com>
2018-11-13 13:57:56 -0600

    Se agregaron dispositivos cercanos

--- index.php
+++ index.php
@@ -1,134 +1,112 @@
 <?php
-/**
-*Load necessary Unifi files
-*/
+
 require('vendor/autoload.php');
 
+require('config.php');
 
-$openFile = fopen('output.txt', 'r');
+$filePath = 'output.txt';
 
-while ($line = fgets($openFile)){
-  echo($line);
-}
+ftruncate($filePath);
 
-echo $probeFile;
 
-foreach ($probeFile as $devices)
-{
-  echo $devices;
-}
-/**
-*Define Unifi login credentials
-*/
-$site_id            = 'default';
 
-$controlleruser     = 'admin';
 
-$controllerpassword = 'Smhau$31.%';
-
-$controllerurl      = 'https://unifi.smarthaus.com.mx:8443';
-
-#--------------------------Connections------------------------------------
-
-/**
-*Create Unifi Connection and Login
-*/
 $unifidata = new UniFi_API\Client($controlleruser, $controllerpassword,
 $controllerurl, $site_id);
+
 $login  = $unifidata->login();
 
-/**
-*Request the selected array from Unifi cloud API
-*Decode and encode it into a Json object.
-*/
-$unifiUsers;
-
-unset($unifiUsers);
-#----------List_Users-------------------------
-
-/**Clear array from previous fetch
-*/
-
-
-/**Stat array from the current selection from Unifi site
-*/
+unset($usersJson);
 
 $unifiUsers = $unifidata->list_clients();
 
-/**Main function
-*/
+$usersJson = json_decode(json_encode($unifiUsers), true);
 
-function showClients($thisJson){
-
-
-/*Decode and encode the current array
-*/
-  $usersJson = json_decode(json_encode($thisJson), true);
-
-  /**save the encoded array into a new variable for convenience
-  */
-  $indexUsers = $usersJson;
-
-  /**Key we want to search in the current array
-  */
-  $hostname = "hostname";
-
-  /**Counter we are using to print in the list
-  */
-  $counter = 0;
-
-  /**We loop times amount of the counter.
-  */
-  for($i = 0; $i <= count($indexUsers) ; $i ++ ){
-
-  /**Select array indentation from the current number in the loop
-  */
-    $printName = $indexUsers[$i];
-
-  /**We start looping through every indentation level in the array
-  */
-    foreach($printName as $key => $value){
-
-  /**if the key is equal as the string defined at $hostname variable, print
-  *current number in the new list, its key and value
-  */
-      if($key == $hostname){
-        $counter ++;
-        $keys = $key;
-        $values = strval($value);
-      }
-    }
-    foreach($printName as $keyRes => $valueRes){
-
-  /**search for the manufacturer name of each device and print it in a single line
-  */
-      if($keyRes == 'oui'){
-
-        global $result;
-        $result = $counter . ' - '.  $valueRes.  ': ' . $values. '<br>';
-        unset($result);
-      }
-    }
-  }
-}
-showClients($unifiUsers);
-
-header("refresh:1;");
 
 ?>
+
+<!DOCTYPE html>
 
 <html>
   <head>
     <title>SMART DETECTION</title>
   </head>
-  <body>
-    <form action="#" method="post">
+  <style>
+    .main{
+      margin: 0px;
+    }
+    .data-div{
+      border: solid black;
+      width:50%;
+      margin:auto;
 
-    </form>
+    }
+
+    .titles
+    {
+      text-align: center;
+      margin: auto;
+      width: 50%;
+    }
+    .content-wrapper{
+      text-align: center;
+      margin:5%;
+    }
+  </style>
+  <body>
     <div class="main">
       <div class="wrapper">
-        <div class="clients">
-          <pre><?php echo $result; ?></pre>
+        <div class="clients data-div">
+          <h3 class='titles'>DISPOSITIVOS CONECTADOS</h3>
+          <div class='content-wrapper'>
+          <?php
+            $counter = 0;
+            for($i = 0; $i <= count($usersJson); $i ++ ){
+
+              $printName = $usersJson[$i];
+
+              foreach($printName as $key => $value){
+
+                if($key == 'hostname'){
+                $values = strval($value);
+                }
+              }
+
+              foreach($printName as $keyRes => $valueRes){
+
+                if($keyRes == 'oui'){
+                  $counter ++;
+                  global $result;
+                  $result = $counter . ' - '.  $valueRes.  ': ' . $values. '<br>';
+                  echo '<pre>'. $result. '</pre>';
+                }
+              }
+            }
+          ?>
+        </div>
+        </div>
+        <div class="near-devices data-div">
+          <h3 class = 'titles'>DISPOSITIVOS CERCANOS</h3>
+          <div class = 'content-wrapper'>
+          <?php
+            $openFile = file('output.txt');
+
+            $devicesJson = array($openFile);
+
+
+            for ($s = 0; $s < count($devicesJson); $s ++ )
+            {
+              $devicesName = $devicesJson[$s];
+
+              foreach($devicesName as $lines => $valueLine)
+              {
+                echo '<pre>' . $valueLine . '</pre>';
+              }
+            }
+          ?>
+          </div>
+        </div>
+        <div class="known-devices">
         </div>
       </div>
     </div>
