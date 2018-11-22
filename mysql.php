42306efdd69705b67c9279fb229bf04c28ec85a4
IsaacDdR <isaacovic00@gmail.com>
2018-11-22 13:16:35 -0600

    Conexion Mysql

---
+++ mysql.php
@@ -0,0 +1,50 @@
+<?php
+
+$servername = 'localhost';
+
+$username   = 'unifi';
+
+$password   = 'TEST';
+
+$db = "unifi";
+
+$conn = mysqli_connect($servername, $username, $password, $db);
+
+if(!$conn){
+  die("Fallo en la conexion " . mysqli_connect_error());
+}
+echo "Conexion exitosa";
+
+/*CREATE DATABASE
+
+$sql = "CREATE DATABASE unifi";
+
+if(mysqli_query($conn, $sql)){
+  echo "Base de datos creada";
+}else{
+  echo "Error al crear la base de datos" . mysqli_error($conn);
+}
+*/
+
+
+/*CREATE TABLE
+
+$sql = "CREATE TABLE unifiDevices(
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  firstname VARCHAR(30) NOT NULL,
+  lastname VARCHAR(30) NOT NULL,
+  email VARCHAR(50),
+  reg_date TIMESTAMP
+)";
+
+if(mysqli_query($conn, $sql)){
+  echo "Tabla creada con exito";
+}else{
+  echo "Error al crear la tabla" . mysql_error($conn);
+}
+/*
+
+
+
+
+?>
